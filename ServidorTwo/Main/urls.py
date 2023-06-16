35ab7c5fbd21a5c36a4cfb19a5a72e874f3f5eeb
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-06-16 15:56:26 -0600

    Se configuro la app y vista de Servidor Two

--- ServidorTwo/Main/urls.py
+++ ServidorTwo/Main/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
+app_name = 'main'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Main.urls'))
+    path('', views.IndexView, name="IndexView")
 ]

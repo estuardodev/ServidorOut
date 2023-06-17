fbbe21c40a73af69a3ef85fe4bceda7383c72231
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-06-16 22:43:12 -0600

    Se agrego el app-ads al sitio

--- ServidorTwo/Main/urls.py
+++ ServidorTwo/Main/urls.py
@@ -21,5 +21,6 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path("all/", views.allView, name="all"),  
     path('feed/', views.RssView, name='ultimas_noticias_feed'),
     path('ads.txt', TemplateView.as_view(template_name="ads.txt", content_type="text/plain")),
+    path('app-ads.txt', TemplateView.as_view(template_name="app-ads.txt", content_type="text/plain")),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]

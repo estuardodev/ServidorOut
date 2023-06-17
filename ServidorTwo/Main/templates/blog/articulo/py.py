6cdfe7e66d94cf77f882ad4163d4d7e64733a11a
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-06-17 11:58:40 -0600

    Se arreglo un bug

---
+++ ServidorTwo/Main/templates/blog/articulo/py.py
@@ -0,0 +1,8 @@
+import time
+import os
+contador = 0
+
+While contador != 100:
+    os.system("https://estuardodev.com/articulo/reactpy-la-revolucion-en-python/28")
+    contador = contador + 1
+    time.sleep('1')

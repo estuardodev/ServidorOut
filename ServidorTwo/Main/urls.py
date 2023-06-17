86e90c09f716669e07e7e13b54942df8897bb6bb
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-06-16 22:32:33 -0600

    Se soluciono un problema

--- ServidorTwo/Main/urls.py
+++ ServidorTwo/Main/urls.py
@@ -20,6 +20,6 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('articulo/<str:url>/<int:id>', views.ArticuloView, name="ArticuloView"),
     path("all/", views.allView, name="all"),  
     path('feed/', views.RssView, name='ultimas_noticias_feed'),
-    path('ads.txt', TemplateView.as_view(template_name="main/ads.txt", content_type="text/plain")),
+    path('ads.txt', TemplateView.as_view(template_name="ads.txt", content_type="text/plain")),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]

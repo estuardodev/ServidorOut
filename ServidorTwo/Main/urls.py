3a7efaaa15bcbd9957b00e88d7c05a56fdbb155c
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-06-16 22:24:15 -0600

    Se agrego el ads

--- ServidorTwo/Main/urls.py
+++ ServidorTwo/Main/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.views.generic.base import TemplateView
 
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import MapaDeSitio
@@ -19,5 +20,6 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('articulo/<str:url>/<int:id>', views.ArticuloView, name="ArticuloView"),
     path("all/", views.allView, name="all"),  
     path('feed/', views.RssView, name='ultimas_noticias_feed'),
+    path('ads.txt', TemplateView.as_view(template_name="main/ads.txt", content_type="text/plain")),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]

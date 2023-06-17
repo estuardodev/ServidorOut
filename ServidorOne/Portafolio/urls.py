01511fa183f426a02e4e522ef912fb0f5f8b73bf
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-06-16 19:12:18 -0600

    Se agregaron los robots y sitemap

--- ServidorOne/Portafolio/urls.py
+++ ServidorOne/Portafolio/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler404, handler500
+from django.views.generic.base import TemplateView
 
 from Main.views import AtributionView, Error404View, Error500View
 
@@ -10,6 +11,9 @@ urlpatterns = [
     path('estuardodev_admin_site/', admin.site.urls),
     path('', include('Main.urls')),
     path('terceros/', AtributionView.as_view()),
+
+    path('robots.txt', TemplateView.as_view(template_name="main/robots.txt", content_type="text/plain")),
+    path('sitemap.xml', TemplateView.as_view(template_name="main/sitemap.xml", content_type="text/xml")),
 ]
 
 # Error 404

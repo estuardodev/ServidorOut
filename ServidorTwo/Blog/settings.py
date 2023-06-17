b8b6098190e8033729e04d7c9bd27dd15f6b97a2
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-06-16 19:45:03 -0600

    Se arreglo el blog

--- ServidorTwo/Blog/settings.py
+++ ServidorTwo/Blog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-from .secrets import SK, DEBUG_STATUS, ALLOWED_HOSTS_PAGES, HOST_DB, NAME_DB, PASS_DB, PORT_DB, USER_DB
+from .secrets import SK, DEBUG_STATUS, ALLOWED_HOSTS_PAGES, HOST_DB, NAME_DB, PASS_DB, PORT_DB, USER_DB, C_T_O_Panel
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +29,7 @@ DEBUG = DEBUG_STATUS
 
 ALLOWED_HOSTS = ALLOWED_HOSTS_PAGES
 
+CSRF_TRUSTED_ORIGINS = C_T_O_Panel
 
 # Application definition
 
